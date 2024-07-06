8bcbc94d3a298d2a158d0b2f496b9cb3da0befe7
IvanShklyaruk <shklyaruk.ivan@gmail.com>
2024-07-05 23:40:54 -0400

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import MagicMatchPage from "./pages/MagicMatchPage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <MagicMatchPage />
+      </div>
+    </div>
+  );
+}
+
+export default App;
